4183425e9b68e8c93392fd5bea8d7246a4e6b8ff
grentank <mexmat100394@gmail.com>
2023-07-10 16:50:00 +0300

    added SWC, updated bundle

---
+++ .eslintrc.cjs
@@ -0,0 +1,51 @@
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', 'react-refresh', 'react', 'react-hooks'],
+  rules: {
+    'default-param-last': 'off',
+    'no-void': 'off',
+    'no-param-reassign': 'off',
+    'react/require-default-props': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'consistent-return': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true,
+        allowHigherOrderFunctions: true,
+        allowDirectConstAssertionInArrowFunctions: true,
+      },
+    ],
+    '@typescript-eslint/switch-exhaustiveness-check': 'error',
+    '@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
+    'import/no-extraneous-dependencies': 'off',
+    'no-console': 'off',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  },
+};
