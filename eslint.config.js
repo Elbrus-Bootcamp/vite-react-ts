97f48af959db04f450812b91c5876f7211449a4f
grentank <mexmat100394@gmail.com>
2024-12-17 00:46:38 +0300

    fixed rules and updated package

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import react from 'eslint-plugin-react';
 import elbrusConfig from '@elbrus/eslint-config';
@@ -11,36 +10,32 @@ export default tseslint.config(
   {
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.strictTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
       ...elbrusConfig,
+      tseslint.configs.stylisticTypeChecked,
+      tseslint.configs.strictTypeChecked,
     ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
 
       // Общие правила
-      'class-methods-use-this': 'off',
-      'no-useless-constructor': 'off',
-      'no-unused-vars': 'off',
-      'no-empty-function': 'off',
+      'class-methods-use-this': 'warn',
       'no-console': 'off',
       'default-param-last': 'off',
       'consistent-return': 'warn',
       'no-void': 'off',
+      'no-param-reassign': 'off',
       '@typescript-eslint/switch-exhaustiveness-check': 'error',
       '@typescript-eslint/no-non-null-assertion': 'warn',
       '@typescript-eslint/no-misused-promises': [
@@ -84,13 +79,13 @@ export default tseslint.config(
     },
   },
   {
-    settings: { react: { version: '18.3' } },
     plugins: {
       react,
     },
     rules: {
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
+      'react/no-array-index-key': 'error',
     },
   },
 );
